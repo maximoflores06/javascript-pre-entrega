2cbc4694d9ad688de6870229cb0acfb7b07c4ffe
Maximo Flores <maximoflores0602@gmail.com>
2026-07-16 09:23:02 -0300

    first commit

---
+++ JS/main.js
@@ -0,0 +1,6 @@
+let edad = parseInt(prompt ("ingresa tu edad"))
+console.log (edad)
+let nombre = prompt("ingresa tu nombre")
+console.log (nombre)
+let resultado = 2026 - edad 
+console.log (2026 - edad)
