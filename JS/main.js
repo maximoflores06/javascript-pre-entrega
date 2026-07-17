4a1feadf745fea70446438ca1d9fb0bdf523bc49
Maximo Flores <maximoflores0602@gmail.com>
2026-07-17 00:12:45 -0300

    corrección

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,12 @@
-let edad = parseInt(prompt ("ingresa tu edad"))
-console.log (edad)
-let nombre = prompt("ingresa tu nombre")
-console.log (nombre)
-let resultado = 2026 - edad 
-console.log (2026 - edad)
+let nombre = prompt("ingresa tu nombre");
+
+let mensaje = "tu nombre es " + nombre;
+console.log(nombre);
+
+let edad = parseInt(prompt("ingresa tu edad"));
+mensaje = "tu edad es " + edad;
+console.log(edad);
+
+let resultado = 2026 - edad;
+mensaje = nombre + ", tu año de nacimiento es " + resultado;
+console.log(mensaje);
