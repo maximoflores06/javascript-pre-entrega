5a904af84858c2e11e69c1583c77eef030a0b773
Maximo Flores <maximoflores0602@gmail.com>
2026-07-30 10:09:20 -0300

    pre - entrega 2

--- JS/main.js
+++ JS/main.js
@@ -1,21 +1,34 @@
-let mensaje;
-
-let nombre = prompt("ingresa tu nombre");
-mensaje = "bienvenido " + nombre;
-console.log(mensaje);
+mensaje = "Bienvenido al soporte de socios del club";
 alert(mensaje);
+let usuario = ""
+let contrasenia =""
+while ( usuario !== "Maximo Flores" || contrasenia !== "1234"
+){
+ usuario = prompt("ingresa tu nombre")
+ contrasenia = prompt ("ingresa tu contraseña")
+if (usuario !== "Maximo Flores" || contrasenia !== "1234")
+    mensaje = "Usuario o contraseña incorrectos"
+    alert (mensaje)
+    console.log ("usuario o contraseña incorrectos");
+}
+console.log ("usuario y contraseña correctos")
+mensaje = "BIENVENIDO"
+alert (mensaje);
+let edad = Number(prompt("ingresa tu edad"))
+if (edad >= 18){
+console.log ("el socio es: mayor")
+mensaje = "Eres MAYOR"
+alert (mensaje);
+}
+else if (edad >= 15 && edad <=17){
+    console.log ("el socio es: cadete mayor")
+mensaje = "Eres CADETE MENOR"
+alert (mensaje);
+}
+else {
+    console.log ("el socio es: cadete menor")
+    mensaje = "Eres CADETE MENOR"
+    alert (mensaje);
+}
 
-let edad = parseInt(prompt("ingresa tu edad"));
-mensaje = "tu edad es : " + edad;
-console.log(mensaje);
-alert(mensaje);
 
-let resultado = 2026 - edad;
-mensaje = nombre + ", naciste en " + resultado;
-console.log(mensaje);
-alert(mensaje);
-
-let Localidad = prompt("¿De qué país eres?");
-mensaje = "Perfecto, te recomendaremos sobre " + Localidad;
-console.log(mensaje);
-alert(mensaje);
