f9a8c697ebfe6f3b4d01abc002439bba0ef1bca8
Maximo Flores <maximoflores0602@gmail.com>
2026-08-06 10:59:13 -0300

    pre-entrega 4

--- JS/main.js
+++ JS/main.js
@@ -1,48 +1,40 @@
 function inicio (){
     let nombre = prompt("ingresa tu nombre")
     alert  ("bienvenido " + nombre)
+    console.log (nombre + " ingresó a la página")
 }
-inicio ()
-function sumar (a, b){
-    let resultado = a + b;
-    return resultado;
+inicio()
+
+const tareas = ["estudio", "cocinar", "hacer las compras","limpiar habitaciones", "preparar ropa de trabajo"]
+console.log (tareas.join (","))
+
+const agreagarfinal = tareas.push ("Poner ropa a lavar")
+console.log ("se añadió una tarea")
+console.log(tareas.join (","))
+
+let agregarinicio = tareas.unshift ("Darle comida al perro")
+console.log ("se agregó una tarea al inicio de la lista")
+console.log (tareas.join (","))
+
+const eliminar = tareas.pop ()
+console.log ("se eliminó la tarea " + eliminar)
+console.log (tareas.join (","))
+
+let buscar = prompt ("Busque su tarea: ")
+console.log (tareas.includes(buscar))
+if (tareas.includes(buscar)){
+    console.log ("la tarea " + buscar + " está en la lista " + "en la posición " + tareas.indexOf(buscar))
+    alert ("la tarea " + buscar + " está en la lista " + "en la posición " + tareas.indexOf(buscar))
 }
-const restar = (a, b) => a - b
-const dividir = (a, b) => a / b
+else{
+    console.log ("la tarea " + buscar + " no está en la lista" )
+    alert ("la tarea " + buscar + " no está en la lista")
+}
+console.log ("se cambió la tarea por Limpiar patio")
+tareas.splice (1, 1, "limpiar patio")
+console.log (tareas.join (","))
 
-
-let operación = Number (prompt(
-    "que operación realizaras?\n" +
-    "1.suma\n" +
-    "2.resta \n" +
-    "3.división\n"+
-    "0.salir "
-)
-);
-while (operación != 0){
-let nro1 = Number (prompt ("ingrese el valor 1"))
-let nro2 = Number (prompt ("ingrese el valor 2"))
-
-switch (operación) {
-    case 1: console.log(sumar (nro1, nro2))
-    alert ("tu resultado es " + sumar (nro1, nro2))
-    break;
-    case 2: console.log(restar (nro1, nro2))
-     alert ("tu resultado es " + restar (nro1, nro2))
-    break;
-    case 3: console.log(dividir(nro1,nro2))
-    alert ("tu resultado es " +dividir(nro1,nro2))
-    default:
+for (const tarea of tareas){
+    console.log (" tarea a realizar : " + tarea)
 }
 
-operación = Number (prompt(
-    "que operación realizaras?\n" +
-    "1.suma\n" +
-    "2.resta \n" +
-    "3.división\n"+
-    "0.salir "
-)
-);
-}
-console.log ("saliste del programa")
-alert ("Saliste del programa");
