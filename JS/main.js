58fb7834914eaa7e5dee0a65e0f1b737b6f0383f
Maximo Flores <maximoflores0602@gmail.com>
2026-09-15 10:38:57 -0300

    pre-entrega 9

--- JS/main.js
+++ JS/main.js
@@ -1,43 +1,28 @@
+const productos = ["mouse", "teclados", "auriculares"]
 
-  function inicio () {
-    let name = prompt("Ingresa tu nombre")
-    alert ("Hola " + name + " Bienvenido a nuestra perfumería ")
+setTimeout (() => alert ("20% off en auriculares"),5000)
+
+localStorage.setItem ("carrito", JSON.stringify(productos))
+
+function obternerCarrito() {
+
+try {
+       const carrito = localStorage.getItem("carrito");
+
+    if (carrito == null) {
+        return [];
+    } else {
+        return JSON.parse(carrito);
+    }
+
+}catch (error) {
+    console.error("Error al obtener el carrito:");
+    return [];
+} finally {
+    console.log("Operación de obtención del carrito finalizada.");
+}
 }
 
-inicio()
+const carrito = obternerCarrito();
+console.log("su carrito contiene: ", carrito);
 
-const perfumes = [
-    { nombre: "Lattafa Khamarah", precio: 89000, categoría: "árabes", tamaño: "50ml" },
-    { nombre: "Lataffa Asad Edp", precio: 41000, categoría: "árabes", tamaño: "50ml" },
-    { nombre: "Lattafa Yara", precio: 40000, categoría: "árabes", tamaño: "100ml" },
-    { nombre: "Good girl", precio: 298000, categoría: "Carolina Herrera", tamaño:"80ml" },
-    { nombre: "212 vip rosé", precio: 157000, categoría: "Carolina Herrera", tamaño:"50ml" },
-    { nombre: "Acqua di gio tradicional", precio: 297000, categoría: "Giorgio Armani", tamaño:"100ml" }
-];
-perfumes.forEach((perfume, index) => {
-    console.log(index + " - perfume: " + perfume.nombre);
-});
-
-const preciodolar = perfumes.map((perfume) => {
-    return {nombre: perfume.nombre, precio: perfume.precio / 1500, categoría: perfume.categoría, tamaño: perfume.tamaño};
-});
-console.table(perfumes);
-
-let búsqueda = prompt(
-"Buscá tu perfume"
-)
-let perfumeEncontrado = perfumes.find (
-    (perfumes) => 
-        perfumes.nombre.toUpperCase().trim() == búsqueda.toUpperCase().trim()
-);
-
-if ( perfumeEncontrado){
-   console.log("El perfume está disponible y su precio es: $" + perfumeEncontrado.precio )
-    console.log("Buscar en el catálogo su valor USD")
-    console.table (preciodolar)
-}else {
-    console.log("perfume no disponible")
-}
-
-const carrito = perfumes.reduce((total, perfumes) => total + perfumes.precio, 0);
-console.log("El total de tu compra es: $" + carrito)
